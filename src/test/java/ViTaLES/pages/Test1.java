f23985fd962049344e4d3d29b9128954c0d8c742
ViTaLES <vitalbka.91@gmail.com>
2015-04-07 19:14:04 +0300

    commit check first test

---
+++ src/test/java/ViTaLES/pages/Test1.java
@@ -0,0 +1,13 @@
+package ViTaLES.pages;
+
+import org.junit.Test;
+
+/**
+ * Created by ViTaLES on 07.04.2015.
+ */
+public class Test1 {
+    @Test
+    public void tests() {
+        System.out.println(1);
+    }
+}
